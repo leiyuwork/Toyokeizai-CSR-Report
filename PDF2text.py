34365516e9fb19ed075ab164f7a8645873b5145d
leiyuwork <leiyuwork@gmail.com>
2020-11-10 21:28:14 +0900

    home pc path change

--- PDF2text.py
+++ PDF2text.py
@@ -4,8 +4,8 @@
 import pdfplumber
 import os
 
-path_in = r"C:\Users\Ray94\OneDrive\test\in\\"  # Original CSR PDF file path
-path_text = r"C:\Users\Ray94\OneDrive\test\text\\"  # Afterwards text file path
+path_in = r"C:\Users\ray\OneDrive\test\in\\"  # Original CSR PDF file path
+path_text = r"C:\Users\ray\OneDrive\test\text\\"  # Afterwards text file path
 
 
 infiles = os.listdir(path_in)
@@ -13,30 +13,30 @@ for infile in infiles:
     text = []  # buckets for text
     with pdfplumber.open(path_in + infile) as pdf:  # open all the files in Original CSR PDF file path
         for page_num in range(len(pdf.pages)):  # crop according the different page number
-            print("*********************"+str(page_num+1)+"********************************")  # from page 1 odd
+            #print("*********************"+str(page_num+1)+"********************************")  # from page 1 odd
             if (page_num % 2) == 0:
                 page = pdf.pages[page_num]
 
                 left = page.crop((0, 0, 252, 668))  # odd page, left side
-                print("*********************" + str(page_num + 1) + "left ********************************")
-                print(left.extract_text())
+                #print("*********************" + str(page_num + 1) + "left ********************************")
+                #print(left.extract_text())
                 text.append(left.extract_text())
 
                 right = page.crop((252, 0, page.width, page.height))  # odd page, right side
-                print("*********************" + str(page_num + 1) + "right ********************************")
-                print(right.extract_text())
+                #print("*********************" + str(page_num + 1) + "right ********************************")
+                #print(right.extract_text())
                 text.append(right.extract_text())
             else:  # page 2 even
                 page = pdf.pages[page_num]
 
                 left = page.crop((0, 0, 262, 668))   # even page, left side
-                print("*********************" + str(page_num + 1) + "left ********************************")
-                print(left.extract_text())
+                #print("*********************" + str(page_num + 1) + "left ********************************")
+                #print(left.extract_text())
                 text.append(left.extract_text())
 
                 right = page.crop((262, 0, page.width, page.height))  # even page, right side
-                print("*********************" + str(page_num + 1) + "right ********************************")
-                print(right.extract_text())
+                #print("*********************" + str(page_num + 1) + "right ********************************")
+                #print(right.extract_text())
                 text.append(right.extract_text())
 
     # write text to txt.file
