e5d54259a88fd9635949d3fc711f556deccd7fd9
leiyuwork <leiyuwork@gmail.com>
2020-11-13 01:31:34 +0900

    pythonic multiple re pattern

--- expression_matching.py
+++ expression_matching.py
@@ -5,8 +5,10 @@ from openpyxl import load_workbook  # A Python library to read/write Excel xlsx/
 path_text = r'C:\Users\ray\OneDrive\test\text\\'
 
 files = os.listdir(path_text)  # txt file path
-pattern_1 = re.compile(r'人材活用 環境 企業統治 社会性\s.{4,20}\s[^a-zA-Z]{4,20}')  # re pattern 01
-pattern_2 = re.compile(r'人材活用 環境 企業統治 社会性\s.{4,20}')  # re pattern 02
+pattern_1 = r'人材活用 環境 企業統治 社会性\s.{4,20}\s.{4,24}\s CSR基本評価 \s基本\s.{1,4}\s.{1,5}\s 財務評価 \s成長性 収益性 安全性 規模\s.{4,20}\s.{4,24}'  # re pattern 01
+pattern_2 = r'人材活用 環境 企業統治 社会性\s.{4,20}\s CSR基本評価 \s基本\s.{1,4}\s.{1,5}\s 財務評価 \s成長性 収益性 安全性 規模\s.{4,20}\s.{4,24}'  # re pattern 02
+pattern_3 = r'人材活用 環境 企業統治 社会性\s.{4,20}\s.{4,24}\s CSR基本評価 \s基本\s.{1,4}\s 財務評価 \s成長性 収益性 安全性 規模\s.{4,20}\s.{4,24}'  # re pattern 03
+pattern_4 = r'人材活用 環境 企業統治 社会性\s.{4,20}\s CSR基本評価 \s基本\s.{1,4}\s 財務評価 \s成長性 収益性 安全性 規模\s.{4,20}\s.{4,24}'  # re pattern 04
 
 for textfile in files:  # for each txt file
     print(textfile)  # confirm file name
@@ -24,20 +26,16 @@ for textfile in files:  # for each txt file
         f.seek(0)  # read txt file again by other read method
 
         data = f.read()  # read whole txt
-        result = pattern_1.findall(data)  # match pattern 01
-
-        if len(result) == 0:  # if no result for pattern 01
-            result = pattern_2.findall(data)  # then try another pattern
-            if len(result) == 0:  # if still no result
-                print("matching result in" + textfile + 'is null or need a different pattern')
-                result.append('null or different pattern')
-                text.append(result[0])
-            else:  # if pattern 02 succeeded
-                text.append(result[0])  # append matching result(02)
-        else:  # if pattern 01 succeeded
+        result = re.compile("(%s|%s|%s|%s)" % (pattern_1, pattern_2, pattern_3, pattern_4)).findall(data)
+        if len(result) == 0:  # if no result
+            print("matching result in " + textfile + 'is null or need a different pattern')
+            result.append('null or different pattern')
+            text.append(result[0])
+        else:  # if pattern 01-04 succeeded
             text.append(result[0])  # append matching result(02)
 
         print(text)  # confirm the matching result
+
         wb = load_workbook(r'C:/Users/ray/OneDrive/test/excel/appending.xlsx')  # open excel
         sheet = wb.active  # get the currently active sheet
         sheet.append(text)  # append matching result to excel sheet
