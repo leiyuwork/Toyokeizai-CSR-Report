ca8f13f6b3a53abf77a62e63207320253c54ac91
leiyuwork <leiyuwork@gmail.com>
2020-11-29 23:40:36 +0900

    2007-2010 pdf2text complete(cropped version)

--- 2007-2010/PDF2text_OCR.py
+++ 2007-2010/PDF2text_OCR.py
@@ -12,11 +12,19 @@ if path_tesseract not in os.environ["PATH"].split(os.pathsep):
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
-# 3.原稿画像の読み込み
+# 3.pdf原稿から画像への変換
 images = convert_from_path(r'C:\Users\ray\Desktop\000\in\2007年 サントリー.pdf', poppler_path=r"C:\Program Files\poppler-\bin")
+print(images)
 for image in images:
     # 4.ＯＣＲ実行
-    builder = pyocr.builders.TextBuilder()
-    result = tool.image_to_string(image, lang="jpn", builder=builder)
 
+    image_left = image.crop((0, 0, 715.5, 2023))
+    builder = pyocr.builders.TextBuilder()
+    result_left = tool.image_to_string(image, lang="jpn", builder=builder)
+
+    image_right = image.crop((715.5, 0, 1431, 2023))
+    builder = pyocr.builders.TextBuilder()
+    result_right = tool.image_to_string(image, lang="jpn", builder=builder)
+
+    result = result_left + result_right
     print(result)
