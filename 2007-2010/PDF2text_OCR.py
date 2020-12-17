bc3dfd1a6aa8278ee2089a3f348cbcb321dac5d1
leiyuwork <g51583255>
2020-12-17 21:31:01 +0900

    新电脑 循环文件夹 2007年

--- 2007-2010/PDF2text_OCR.py
+++ 2007-2010/PDF2text_OCR.py
@@ -2,6 +2,8 @@ import os
 import pyocr.builders
 from pdf2image import convert_from_path
 
+path_in = r"C:\Users\Ray94\Downloads\in\\"  # Original CSR PDF file path
+path_text = r"C:\Users\Ray94\Downloads\out\\"  # Text file path
 
 # 1.インストール済みのTesseractのパスを通す
 path_tesseract = r"C:\\Program Files (x86)\\Tesseract-OCR"
@@ -13,18 +15,25 @@ tools = pyocr.get_available_tools()
 tool = tools[0]
 
 # 3.pdf原稿から画像への変換
-images = convert_from_path(r'C:\Users\ray\Desktop\000\in\2007年 サントリー.pdf', poppler_path=r"C:\Program Files\poppler-\bin")
-print(images)
-for image in images:
-    # 4.ＯＣＲ実行
+infiles = os.listdir(path_in)
+for infile in infiles:
+    text = []
+    print(infile)
+    images = convert_from_path(path_in+infile, poppler_path=r"C:\Program Files\poppler-\bin")
 
-    image_left = image.crop((0, 0, 715.5, 2023))
-    builder = pyocr.builders.TextBuilder()
-    result_left = tool.image_to_string(image, lang="jpn", builder=builder)
+    for image in images:
+        # 4.ＯＣＲ実行
 
-    image_right = image.crop((715.5, 0, 1431, 2023))
-    builder = pyocr.builders.TextBuilder()
-    result_right = tool.image_to_string(image, lang="jpn", builder=builder)
+        image_left = image.crop((0, 0, 715.5, 2023))
+        builder = pyocr.builders.TextBuilder()
+        result_left = tool.image_to_string(image_left , lang="jpn", builder=builder)
+        text.append(result_left)
 
-    result = result_left + result_right
-    print(result)
+        image_right = image.crop((715.5, 0, 1431, 2023))
+        builder = pyocr.builders.TextBuilder()
+        result_right = tool.image_to_string(image_right, lang="jpn", builder=builder)
+        text.append(result_right)
+
+    with open(path_text + infile + ".txt","wb+") as f:
+        for t in text:
+            f.write(str(t).encode('utf8'))
